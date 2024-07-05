51247f02c78a09a6e42d68f2465a0cc9f00c9792
Sleeper9235 <kylejyoumans@gmail.com>
2024-07-05 15:41:38 -0500

    MVP met

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ const methodOverride = require('method-override');
 const morgan = require('morgan');
 const session = require('express-session');
 
+const isSignedIn = require('./middleware/is-signed-in.js')
+const passUserToView = require('./middleware/pass-user-to-view.js')
+
 const authController = require('./controllers/auth.js');
+const listingsController = require('./controllers/listings');
 
 const port = process.env.PORT ? process.env.PORT : '3000';
 
@@ -15,11 +19,11 @@ mongoose.connect(process.env.MONGODB_URI);
 
 mongoose.connection.on('connected', () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
-});
+});  
 
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
-// app.use(morgan('dev'));
+app.use(morgan('dev'));
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
@@ -28,21 +32,24 @@ app.use(
   })
 );
 
+app.use(passUserToView)
+
 app.get('/', (req, res) => {
   res.render('index.ejs', {
     user: req.session.user,
   });
 });
 
-app.get('/vip-lounge', (req, res) => {
-  if (req.session.user) {
-    res.send(`Welcome to the party ${req.session.user.username}.`);
-  } else {
-    res.send('Sorry, no guests allowed.');
-  }
-});
+// app.get('/vip-lounge', (req, res) => {
+//   if (req.session.user) {
+//     res.send(`Welcome to the party ${req.session.user.username}.`);
+//   } else {
+//     res.send('Sorry, no guests allowed.');
+//   }
+// });
 
 app.use('/auth', authController);
+app.use('/listings', isSignedIn, listingsController)
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
